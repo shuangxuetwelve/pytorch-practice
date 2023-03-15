67dfd7a4f3893746dc13c2ea26d04a7e4b382479
shuangxuetwelve <77678255+shuangxuetwelve@users.noreply.github.com>
2023-03-15 21:03:54 +0800

    Cache dataset to accelerate training

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
-# Python imports.
+# Python imports
 import random
 import time
+
+# PyTorch imports
+import torchvision
 
 def sample_MNIST_with_all_labels(dataset):
     images = {}
@@ -27,3 +30,27 @@ class Timer():
     # Get the duration in seconds.
     def getDuration(self):
         return self.end_time - self.start_time    
+    
+class MNIST(torchvision.datasets.MNIST):
+
+    cache = {}
+
+    def __init__(self, path, train=True, transform=None):
+
+        super().__init__(path, download=True, train=train, transform=transform)
+
+        # Access all samples to pre-cache them.
+        for idx in range(self.__len__()):
+            self.__getitem__(idx)
+
+    def __len__(self):
+
+        return super().__len__()
+    
+    def __getitem__(self, idx):
+
+        if idx in self.cache:
+            return self.cache[idx]
+        else:
+            self.cache[idx] = super().__getitem__(idx)
+            return self.cache[idx]
